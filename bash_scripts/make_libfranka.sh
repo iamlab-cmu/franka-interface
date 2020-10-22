eca813db4b4d311592e2f1d41fb543c100dfb7e6
Jacky Liang <jackyliang@cmu.edu>
2020-10-22 18:32:38 -0400

    automatically detect test line number in make libfranka

--- bash_scripts/make_libfranka.sh
+++ bash_scripts/make_libfranka.sh
@@ -1,7 +1,9 @@
 cd libfranka
 
+test_line_number=$(grep -n "Build tests" CMakeLists.txt | cut -f1 -d:)
+
 # Change built test to Off
-sed -i '155s/.*/option(BUILD_TESTS "Build tests" OFF)/' CMakeLists.txt
+sed -i $test_line_number's/.*/option(BUILD_TESTS "Build tests" OFF)/' CMakeLists.txt
 
 # Get CPU core count
 n_cores=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
