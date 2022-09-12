f9c61522c36a9491cb1ace3d78f2b6fabd415e0c
Kevin Zhang <kevinleezhang@gmail.com>
2022-09-12 17:57:31 +0900

    Updated libfranka version for ros2 humble.

--- bash_scripts/clone_libfranka.sh
+++ bash_scripts/clone_libfranka.sh
@@ -1,29 +1,6 @@
-firmware_version=$1
+rm -rf libfranka
+git clone https://github.com/frankaemika/libfranka.git
+cd libfranka
+git checkout f3b8d77
+git submodule update --init --recursive
 
-valid_version=true
-case $firmware_version in
-2)
-  commit=5ca631e
-  ;;
-3)
-  commit=80197c6
-  ;;
-4)
-  commit=f1f46fb
-  ;;
-5)
-  commit=c452ba2
-  ;;
-*)
-  valid_version=false
-  echo "Please enter your robot's firmware version as 2, 3, 4, or 5."
-  ;;
-esac
-
-if [ "$valid_version" = true ] ; then
-  rm -rf libfranka
-  git clone https://github.com/frankaemika/libfranka.git
-  cd libfranka
-  git checkout $commit
-  git submodule update --init --recursive
-fi
